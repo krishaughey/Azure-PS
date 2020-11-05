66b0c4620d49f292a505adc2f65bf931711f9f93
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2020-11-05 16:21:43 -0600

    DL Membership

---
+++ DL_GetAllWithMembership.ps1
@@ -0,0 +1,18 @@
+# Get All O365 Distribution Groups with Membership
+##### author: Kristopher F. Haughey
+
+
+$Groups = Get-DistributionGroup -ResultSize Unlimited
+$Groups | ForEach-Object {
+$group = $_.Name
+Get-DistributionGroupMember $group -ResultSize Unlimited | ForEach-Object {
+      New-Object -TypeName PSObject -Property @{
+       Group = $group
+       Member = $_.Name
+       EmailAddress = $_.PrimarySMTPAddress
+       RecipientType= $_.RecipientType
+      }
+   }
+} |
+ 
+Export-CSV "C:\Temp\O365_DLMembership_20201029.csv" -NoTypeInformation
